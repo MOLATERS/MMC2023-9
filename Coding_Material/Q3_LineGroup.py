4c103ea28a71aff959dfe4995b69d8be22a0a66a
MOLATERS <1643381107@qq.com>
2023-09-09 15:18:58 +0800

    2023.9.9\

--- Coding_Material/Q3_LineGroup.py
+++ Coding_Material/Q3_LineGroup.py
@@ -1,18 +1,81 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Q3_basic_math import calculate_Wn_1
-import math
+from math import *
+from scipy.optimize import fsolve
+
+import random
+# 用于记录每一个y_i的位置
+line_positions = [0]
+L = 1852 * 4
+gamma = sin(np.radians(31.5)) / (sin(np.radians(31.5)) + sin(np.radians(28.5)))
+step = 0.02*1852
+simulate_times = 100
 
 # 设置宽度左右两边的比例关系
-west_width_percent = math.sin(math.radians(61.5))/(math.sin(math.radians(61.5))+math.sin(math.radians(58.5)))
- 
+west_width_percent = sin(radians(61.5))/(sin(radians(61.5))+sin(radians(58.5)))
+
 # 设置计算出来的宽度
-def dedect_width(y_position):
+def detect_width(y_position):
     return calculate_Wn_1(y_position)
 
-# 计算出可能的n的数量
-def Calculate_lines():
-    yn_1_west_decide = 0;
-    yn_west_decide = 0;
-    # while yn_1_west_decide < 4 * 1825:
-        
+
+# 从前向后算
+def Solvevalue_1(unsolvedvalue,edge):
+    y = unsolvedvalue[0]
+    edge = edge
+    return [
+        detect_width(y)*gamma - y + edge,
+    ]
+def Calculate_lines_1(y):
+    line_positions[0]=y
+    while line_positions[-1] <= L:
+        width = detect_width(line_positions[-1])
+        edge = line_positions[-1]+width*(1-gamma-0.1)
+        # print(detect_width(edge)*gamma - edge)
+        Solvevalue_1([0],edge)
+        y_next = fsolve(Solvevalue_1,[0],args=edge)
+        line_positions.append(y_next[0])
+    temp1 = line_positions[len(line_positions)-2]
+    temp2 = line_positions[-1]
+    flag_last = temp1 + detect_width(temp1)*(1-gamma)
+    flag_next = temp2 - detect_width(temp2)*(gamma)
+    ita = (flag_last-flag_next)/ detect_width(temp1)
+    return ita,temp1,temp2,flag_last,flag_next
+
+# 从后向前算
+def Solvevalue_2(unsolvedvalue,edge):
+    y = unsolvedvalue[0]
+    edge = edge
+    return [
+        detect_width(y)*(1-gamma) + y - edge,
+    ]
+def Calculate_lines_2(y):
+    line_positions[0]=y
+    while line_positions[-1] >= 0:
+        width = detect_width(line_positions[-1])
+        edge = line_positions[-1]-width*(gamma-0.1)
+        print(edge)
+        Solvevalue_2([0],edge)
+        y_next = fsolve(Solvevalue_2,[0],args=edge)
+        line_positions.append(y_next[0])
+    temp1 = line_positions[len(line_positions)-2]
+    temp2 = line_positions[-1]
+    flag_last = temp1 - detect_width(temp1)*gamma
+    flag_next = temp2 + detect_width(temp2)*(1-gamma)
+    ita = (flag_next-flag_last)/ detect_width(temp1)
+    return ita,temp1,temp2,flag_last,flag_next
+
+
+Solvevalue_1([0],0)
+solved = fsolve(Solvevalue_1,[0],args=0)
+print(solved)
+print(Calculate_lines_1(solved[0]))
+plt.plot(line_positions)
+
+line_positions=[0]
+Solvevalue_2([0],L)
+solved = fsolve(Solvevalue_2,[0],args=L)
+print(solved)
+print(Calculate_lines_2(solved[0]))
+plt.plot(line_positions)
--- Coding_Material/Q3_basic_math.py
+++ Coding_Material/Q3_basic_math.py
@@ -5,7 +5,17 @@ import math
 def calculate_Wn_1(y_position):
     theta = math.radians(120)
     alpha = math.radians(1.5)
-    W = math.sin(theta) * math.cos(alpha)**2 * (70 - y_position * math.tan(alpha)) / (math.cos(theta/2-alpha) * math.cos(theta/2+alpha))
+    Depth = 2 * 1852 * math.tan(math.radians(1.5)) + 110 - y_position * math.tan(math.radians(1.5))
+    # print(Depth)
+    W = math.sin(theta) * math.cos(alpha)**2 * Depth / (math.cos(theta/2-alpha) * math.cos(theta/2+alpha))
     return W
-# W = math.sin(theta) * math.cos(alpha)**2 * (70 - y_position * math.tan(alpha)) / (math.cos(theta/2-alpha) * math.cos(theta/2+alpha))
 
+# print(calculate_Wn_1(2*1852))
+
+
+#记录每单位的移动对应的测量数量
+# def detect_array():
+#     detect_array = []
+#     for i in range(0,L,step):
+#         detect_array.append((i,detect_width(i)))
+#     return detect_array
